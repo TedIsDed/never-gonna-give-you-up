94d657d349ffbc5bd62ba4b021bfc0d4003243bd
TedIsDed <114429467+TedIsDed@users.noreply.github.com>
2022-10-10 19:43:33 +0300

    just a code to use python

---
+++ idk.py
@@ -0,0 +1,6 @@
+answer = input("hello do u like undertale? (only 'yes' or 'no')")
+if answer == "yes":
+    print("i love undertale too only")
+elif answer == "no":
+    print("how u not like a masterpiece like that")
+else: print("i said u can only type 'yes' or 'no'")
